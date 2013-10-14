0361a377e885d0eb2550272469fddc986f7d97db
RetVal <retval@me.com>
2013-10-14 17:34:50 +0800

    Zan

--- RSRenrenDump/ViewControllers/Zan/RSZhenYiYueViewController.m
+++ RSRenrenDump/ViewControllers/Zan/RSZhenYiYueViewController.m
@@ -12,8 +12,12 @@
 #import "RSRemoteDataBase.h"
 #import "RSBaseModel.h"
 #import "RSLikeModel.h"
+#import "RSProgressHUD.h"
 
 @interface RSZhenYiYueViewController ()
+{
+    RSCoreAnalyzer *_analyzer;
+}
 
 @end
 
@@ -31,6 +35,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    _analyzer = [[RSSharedDataBase sharedInstance] currentAnalyzer];
 	// Do any additional setup after loading the view.
 }
 
@@ -42,5 +47,46 @@
 
 - (IBAction)zanSliderValueChanged:(UISlider *)sender {
     [_itemsNumber setText: [[NSString alloc] initWithFormat:@"%d", (NSUInteger)[sender value]]];
+}
+
+- (IBAction)zan:(id)sender {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [RSProgressHUD showProgress:-1.0f status:@"Zaning..." maskType:RSProgressHUDMaskTypeGradient];
+        NSUInteger begin = 0;
+        NSUInteger limit = 50;
+        NSUInteger count = 0;
+        NSUInteger turn = [[_itemsNumber text] integerValue] / 50 + 1;
+        BOOL shouldContinue = YES;
+        NSMutableArray *models = [[NSMutableArray alloc] init];
+        while (shouldContinue)
+        {
+            if (count > turn) shouldContinue = NO;
+            NSURLResponse *response = nil;
+            NSData *data = nil;
+            NSError *error = nil;
+            //333092533
+            data = [NSURLConnection sendSynchronousRequest:[RSRemoteDataBase remoteUpdateUser:[_model account] count:count analyer:_analyzer] returningResponse:&response error:&error];  // 个人主页 朕已阅
+//            data = [RSRemoteDataBase remoteSyncInvokeUpdatePage:begin limit:limit count:count analyer:_analyzer response:&response error:&error]; // 新鲜事 朕已阅
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            if ([httpResponse statusCode] == 200)
+                [models addObjectsFromArray:[_analyzer analyzeLikeModelWithData:data mode:RSRenrenAddLike]];
+            else
+                shouldContinue = NO;
+            count++;
+            begin += limit;
+            [NSThread sleepForTimeInterval:1.25];
+        }
+        if ([models count])
+        {
+            [models enumerateObjectsUsingBlock:^(RSLikeModel *obj, NSUInteger idx, BOOL *stop) {
+                [obj action];
+                [NSThread sleepForTimeInterval:2];
+            }];
+            [RSProgressHUD dismiss];
+            NSLog(@"end");
+            return ;
+        }
+        [RSProgressHUD dismiss];
+    });
 }
 @end
